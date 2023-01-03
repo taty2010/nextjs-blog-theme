9fe33d406df1802742f67d9bd9dab2ec48bf5f86
taty2010 <shydalys123@gmail.com>
2023-01-03 17:38:00 -0600

    changes baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "88b15q",
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:8888',
     supportFile: false,
   },
 });
